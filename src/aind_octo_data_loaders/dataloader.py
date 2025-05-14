834b409789851250ce69a79a4d02fd48720e8e06
Geoffrey Schau <schau.geoffrey@gmail.com>
2025-05-14 12:23:37 -0700

    unchained worker_init_fn for individual dataloaders

--- src/aind_octo_data_loaders/dataloader.py
+++ src/aind_octo_data_loaders/dataloader.py
@@ -9,6 +9,7 @@ from zarrdataset import (
     ImagesDatasetSpecs,
     PatchSampler,
     ZarrDataset,
+    zarrdataset_worker_init_fn,
     chained_zarrdataset_worker_init_fn,
 )
 
@@ -182,7 +183,7 @@ class ZarrDatasets:
                     dataset,
                     batch_size=self.batch_size,
                     num_workers=self.num_workers,
-                    worker_init_fn=chained_zarrdataset_worker_init_fn,
+                    worker_init_fn=zarrdataset_worker_init_fn,
                     pin_memory=True,
                 )
             for dataset in self.individual_datasets
